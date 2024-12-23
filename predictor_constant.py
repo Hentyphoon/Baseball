1087b78b2ea1348938ce795aed636c345e49f337
Taehun Lee <91559425+Hentyphoon@users.noreply.github.com>
2024-12-23 16:51:13 -0700

    Update predictor_constant.py

--- predictor_constant.py
+++ predictor_constant.py
@@ -1,6 +1,6 @@
 import pybaseball as pyball
 import numpy as np
-
+ # add MBGD and GDB depending on T and n to optimize runtime
 def step_size(x):
   return x
 
@@ -11,9 +11,15 @@ def sigmoid(x,w):
 def sum(x,w,y):
   sum = 0
   for i in range(len(x)):
-    
-
-
+    sig = sigmoid(x[i],w[i])
+    sum += (sig - y[i]) * x[i]
+  return sum
+  
 def mle_predictor(x,y,step_size,T):
+  n, d = X[:,1:].shape
+  w = np.random.randn(d+1)
   for t in T:
-    
+    w -= step_size*sum(x,w,y)
+
+  return w
+  
